92a28d9e4494d666583e7ac17a3ac281a0cd143d
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-21 14:50:32 +0300

    root

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form/Form";
+import List from "./components/List/List";
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Todo App</h2>
+      <List todos={todos} />
+      <Form addTodos={setTodos} todos={todos} />
     </div>
   );
 }
