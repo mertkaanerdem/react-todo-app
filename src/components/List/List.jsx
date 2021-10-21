3c621a159b9f26ba987877eae0bdb1873597e18c
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-21 15:01:26 +0300

    done Item

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,23 @@
-function List({ todos }) {
+function List({ todos, removeItem }) {
+  function doneItem(e) {
+    if (e.target.style.textDecoration !== "line-through") {
+      e.target.style.textDecoration = "line-through";
+      e.target.style.opacity = ".2";
+    } else {
+      e.target.style.textDecoration = "";
+      e.target.style.opacity = "1";
+    }
+    console.log("üstü çizildi");
+  }
+
   return (
     <div>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>
+            <p onClick={doneItem}>{todo}</p>
+            <button onClick={removeItem}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
